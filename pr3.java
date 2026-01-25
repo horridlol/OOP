0b78013c9714d6291759236e6c5e0ea00a1a82e1
horridlol <www.dx328@gmail.com>
2026-01-25 22:42:41 +0530

    v

---
+++ pr3.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class pr3 {
+    public static void main(String[] arg) {
+        int x = Integer.parseInt(arg[0]);
+        int y = Integer.parseInt(arg[1]);
+        System.out.println(x+y);
+    }
+}
