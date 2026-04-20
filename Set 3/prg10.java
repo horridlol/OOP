20f3ee2c43916eb57cf66b0d8eee7424d86ae100
daksh <www.dx328@gmail.com>
2026-04-20 22:37:53 +0530

    Add files via upload

--- Set 3/prg10.java
+++ Set 3/prg10.java
@@ -1,1 +1,30 @@
+class BankAccount{
+    String account_holder_name;
+    double balance;
+    static double interest_rate;
 
+    BankAccount(String name,double bal){
+        account_holder_name=name;
+        balance=bal;
+    }
+
+    static void update_interest(double new_rate){
+        interest_rate=new_rate;
+    }
+
+    void display_interest(){
+        double interest_earned=balance * (interest_rate / 100);
+        System.out.println("Account Holder : "+account_holder_name);
+        System.out.println("Interest earned : "+interest_earned);
+        System.out.println("New Balanace : "+(balance+interest_earned));
+    }
+}
+public class prg10 {
+    public static void main(String[] args) {
+        BankAccount.update_interest(6.4);
+        BankAccount ac1=new BankAccount("Rajesh", 10000);
+        BankAccount ac2=new BankAccount("Nisha", 25000);
+        ac1.display_interest();
+        ac2.display_interest();
+    }
+}
