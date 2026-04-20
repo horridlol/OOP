53b827db8087c9ae96c588e367c51f842c2947a0
daksh <www.dx328@gmail.com>
2026-04-20 18:29:09 +0530

    Add files via upload

--- Set 2/prg6.java
+++ Set 2/prg6.java
@@ -1,1 +1,23 @@
-
+public class prg6{
+    public static void main(String[] args) {
+        Rectangle r=new Rectangle();
+        System.out.println(r.getArea());
+        System.out.println(r.getPerimeter());
+    }
+}
+class Rectangle{
+    double width=1, height=1;
+    Rectangle(){
+        return;
+    }
+    Rectangle(double width,double height){
+        this.width=width;
+        this.height=height;
+    }
+    double getArea(){
+        return width*height;
+    }
+    double getPerimeter(){
+        return 2*(width+height);
+    }
+}
