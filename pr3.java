7c30145d59a0fbaa59f1e933afd9e630e7ec918e
daksh <www.dx328@gmail.com>
2026-04-20 18:16:40 +0530

    Delete pr3.java

--- pr3.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class pr3 {
-    public static void main(String[] arg) {
-        int x = Integer.parseInt(arg[0]);
-        int y = Integer.parseInt(arg[1]);
-        System.out.println(x+y);
-    }
-}
